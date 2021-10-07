be7067e3f838eea57c60ee5153aaa9a6dc030f65
DaveCoded <davidbernhard@live.com>
2021-10-07 21:10:25 +0100

    change blockquote styling

--- components/mdx/typography/Blockquote.tsx
+++ components/mdx/typography/Blockquote.tsx
@@ -1,9 +1,11 @@
 import styled from 'styled-components'
 
 const Blockquote = styled.blockquote`
+    margin: var(--space-xl) 0;
+
     p {
-        font-family: 'Headline';
         font-weight: 700;
+        line-height: 1.3;
         padding: 0 var(--space-md);
         border-left: 4px solid var(--dark-grey);
     }
