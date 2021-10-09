eaff21662fb8647e4b0a1edf0b7522d0b5cf5019
DaveCoded <davidbernhard@live.com>
2021-10-09 19:08:26 +0100

    move post metadata to its own component

---
+++ components/PostMetadata.tsx
@@ -0,0 +1,119 @@
+import styled from 'styled-components'
+import Link from 'next/link'
+import FireLevel, { FireType } from './FireLevel'
+import { timeAgo } from '../lib/dates'
+
+type Props = {
+    date: string
+    completion: FireType
+    updated?: string
+    tags?: string[]
+}
+
+export default function PostMetadata({ tags, completion, date, updated }: Props) {
+    const firstLitDate = new Date(date)
+    const formattedLitDate = firstLitDate.toLocaleDateString('en-US', {
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric'
+    })
+    const lastStoked = (updated && timeAgo(new Date(updated))) || null
+
+    return (
+        <Metadata>
+            <div style={{ display: 'flex', alignItems: 'baseline' }}>
+                Tags:
+                {tags && tags.length > 0 ? (
+                    <TagList>
+                        {tags.map((tag) => (
+                            <Link key={tag} href={`/tag/${tag}`}>
+                                <a>
+                                    <Tag>{tag}</Tag>
+                                </a>
+                            </Link>
+                        ))}
+                    </TagList>
+                ) : null}
+            </div>
+            <InnerMetadata>
+                <FireWrapper>
+                    <FireLevel completion={completion} />
+                </FireWrapper>
+                <Pipe>|</Pipe>
+                <DateWrapper>
+                    <FirstLit>First lit on {formattedLitDate}</FirstLit>
+                    {updated && <span>Last stoked {lastStoked}</span>}
+                </DateWrapper>
+            </InnerMetadata>
+        </Metadata>
+    )
+}
+
+const Metadata = styled.div`
+    display: flex;
+    align-items: baseline;
+    margin-top: var(--space-md);
+    font-weight: 600;
+    font-size: var(--text-sm);
+
+    @media (max-width: 900px) {
+        flex-direction: column;
+    }
+`
+
+const InnerMetadata = styled.div`
+    margin-left: auto;
+    display: flex;
+    align-items: baseline;
+
+    @media (max-width: 900px) {
+        flex-direction: column;
+        margin-left: 0;
+    }
+`
+
+const Tag = styled.li`
+    font-style: italic;
+    font-size: var(--text-sm);
+    font-weight: 600;
+    list-style: none;
+    color: var(--light-black);
+    text-decoration: underline;
+    transition: var(--link-hover-transition);
+
+    &:hover {
+        color: var(--link-pink);
+    }
+`
+
+const FireWrapper = styled.span`
+    margin-left: auto;
+
+    @media (max-width: 900px) {
+        margin-left: 0;
+    }
+`
+
+const Pipe = styled.span`
+    margin: 0 var(--space-xs);
+
+    @media (max-width: 900px) {
+        display: none;
+    }
+`
+
+const DateWrapper = styled.span`
+    display: flex;
+    flex-direction: column;
+    text-align: right;
+`
+
+const FirstLit = styled.span`
+    margin-bottom: var(--space-xxs);
+`
+
+const TagList = styled.ul`
+    margin-left: var(--space-sm);
+    display: flex;
+    gap: var(--space-sm);
+`
--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import { GetStaticProps } from 'next'
-import Link from 'next/link'
 import matter from 'gray-matter'
 import mdxPrism from 'mdx-prism'
 import AllComponents from '../../components/mdx/AllComponents'
@@ -9,14 +8,13 @@ import styled from 'styled-components'
 import { H2 } from '../../components/mdx/typography'
 import { PostData } from '../../types/PostData'
 import PageLayout from '../../components/Layout/PageLayout'
-import FireLevel from '../../components/FireLevel'
-import { timeAgo } from '../../lib/dates'
 import { serialize } from 'next-mdx-remote/serialize'
 import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote'
 import { linkify } from '../../lib/linkify'
 import PostLinks from '../../links.json'
 import Backlinks from '../../components/Backlinks'
 import { LinkReference } from '../../scripts/post-links'
+import PostMetadata from '../../components/PostMetadata'
 
 interface Props {
     source: MDXRemoteSerializeResult<Record<string, unknown>>
@@ -27,7 +25,6 @@ interface Props {
 const components = AllComponents
 
 export default function Posts({ source, frontMatter, backlinks }: Props) {
-    const options = { month: 'long', day: 'numeric', year: 'numeric' } as any
     const {
         title,
         description,
@@ -38,9 +35,6 @@ export default function Posts({ source, frontMatter, backlinks }: Props) {
         completion = 'spark',
         updated
     } = frontMatter
-    const firstLitDate = new Date(date)
-    const formattedLitDate = firstLitDate.toLocaleDateString('en-US', options)
-    const lastStoked = (updated && timeAgo(new Date(updated))) || null
 
     return (
         <>
@@ -54,35 +48,13 @@ export default function Posts({ source, frontMatter, backlinks }: Props) {
                 <PostContainer>
                     <Title hasSubtitle={!!subtitle}>{title}</Title>
                     {subtitle && <Subtitle>{subtitle}</Subtitle>}
-
                     <HR />
-
-                    <Metadata>
-                        <div style={{ display: 'flex', alignItems: 'baseline' }}>
-                            Tags:
-                            {tags && tags.length > 0 ? (
-                                <TagList>
-                                    {tags.map((tag) => (
-                                        <Link key={tag} href={`/tag/${tag}`}>
-                                            <a>
-                                                <Tag>{tag}</Tag>
-                                            </a>
-                                        </Link>
-                                    ))}
-                                </TagList>
-                            ) : null}
-                        </div>
-                        <InnerMetadata>
-                            <FireWrapper>
-                                <FireLevel completion={completion} />
-                            </FireWrapper>
-                            <Pipe>|</Pipe>
-                            <DateWrapper>
-                                <FirstLit>First lit on {formattedLitDate}</FirstLit>
-                                {updated && <span>Last stoked {lastStoked}</span>}
-                            </DateWrapper>
-                        </InnerMetadata>
-                    </Metadata>
+                    <PostMetadata
+                        completion={completion}
+                        date={date}
+                        updated={updated}
+                        tags={tags}
+                    />
 
                     <ContentWrapper>
                         <MDXRemote {...source} components={components} />
@@ -154,75 +126,6 @@ const Subtitle = styled(H2)`
 
 const HR = styled.hr`
     border: 1px solid var(--cool-grey);
-`
-
-const Metadata = styled.div`
-    display: flex;
-    align-items: baseline;
-    margin-top: var(--space-md);
-    font-weight: 600;
-    font-size: var(--text-sm);
-
-    @media (max-width: 900px) {
-        flex-direction: column;
-    }
-`
-
-const InnerMetadata = styled.div`
-    margin-left: auto;
-    display: flex;
-    align-items: baseline;
-
-    @media (max-width: 900px) {
-        flex-direction: column;
-        margin-left: 0;
-    }
-`
-
-const Tag = styled.li`
-    font-style: italic;
-    font-size: var(--text-sm);
-    font-weight: 600;
-    list-style: none;
-    color: var(--light-black);
-    text-decoration: underline;
-    transition: var(--link-hover-transition);
-
-    &:hover {
-        color: var(--link-pink);
-    }
-`
-
-const FireWrapper = styled.span`
-    margin-left: auto;
-
-    @media (max-width: 900px) {
-        margin-left: 0;
-    }
-`
-
-const Pipe = styled.span`
-    margin: 0 var(--space-xs);
-
-    @media (max-width: 900px) {
-        display: none;
-    }
-`
-
-const DateWrapper = styled.span`
-    display: flex;
-    flex-direction: column;
-    text-align: right;
-`
-
-const FirstLit = styled.span`
-    margin-bottom: var(--space-xxs);
-`
-
-const TagList = styled.ul`
-    margin-left: var(--space-sm);
-    display: flex;
-    gap: var(--space-sm);
 `
 
 const ContentWrapper = styled.div`
