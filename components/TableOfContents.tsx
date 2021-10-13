ac16535d1d3bd225dba76e7db75cfa9e8cd8ab30
DaveCoded <davidbernhard@live.com>
2021-10-13 09:01:29 +0100

    link from h2 and h3s toc

--- components/TableOfContents.tsx
+++ components/TableOfContents.tsx
@@ -1,21 +1,31 @@
+import { useRouter } from 'next/router'
+
 type Props = {
     headings: { text: string; level: number }[]
 }
 
 function TableOfContents({ headings }: Props) {
+    const route = useRouter().asPath
+
+    console.log('headings', headings)
     return (
         <div>
-            {headings.map(({ text, level }) => (
-                // todo: add new <ol> for each new nested level
-                // if level is greater than the previous level, add an <ol>
-                <a
-                    key={text}
-                    href={`${text}`}
-                    style={{ display: 'block', marginLeft: level === 2 ? 0 : level * 6 }} // todo: sort this indentation maths
-                >
-                    {text} - {level}
-                </a>
-            ))}
+            {headings.map(({ text, level }) => {
+                const link = text.toLowerCase().replaceAll(' ', '-')
+                const href = `${route}#${link}`
+
+                return (
+                    // todo: add new <ol> for each new nested level
+                    // if level is greater than the previous level, add an <ol>
+                    <a
+                        key={text}
+                        href={href}
+                        style={{ display: 'block', marginLeft: level === 2 ? 0 : level * 6 }} // todo: sort this indentation maths
+                    >
+                        {text} - {level}
+                    </a>
+                )
+            })}
         </div>
     )
 }
