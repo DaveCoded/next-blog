f8625dee8f587a27ef75b016758c0ca8d1b27a2d
Dave Bernhard <davidbernhard@live.com>
2022-07-10 13:24:31 -0400

    remove unused key from page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,13 @@ import MDXProvider from '../components/mdx/MDXProvider'
 import { GlobalStyle } from '../components/GlobalStyle'
 import SiteLayout from '../components/Layout/SiteLayout'
 import './_app.css'
-import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
-    const route = useRouter().route
-
     return (
         <MDXProvider>
             <GlobalStyle />
             <SiteLayout>
-                <Component {...pageProps} key={route} />
+                <Component {...pageProps} />
             </SiteLayout>
         </MDXProvider>
     )
