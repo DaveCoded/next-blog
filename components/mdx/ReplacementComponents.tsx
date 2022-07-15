7e5358108a4ea8356bc0a5a16cbb4cd05fc81ef3
Dave Bernhard <davidbernhard@live.com>
2022-07-14 21:47:17 -0400

    add icons to headings on hover

--- components/mdx/ReplacementComponents.tsx
+++ components/mdx/ReplacementComponents.tsx
@@ -21,6 +21,12 @@ const ImgContainer = styled.div`
     margin-bottom: 1.5rem;
 `
 
+const HeadingLink = styled.a`
+    &:hover {
+        color: currentColor;
+    }
+`
+
 const wrapHeadingInLink = (
     // eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any
     Heading: StyledComponent<'h2' | 'h3' | 'h4' | 'h5', any, {}, never>,
@@ -29,7 +35,7 @@ const wrapHeadingInLink = (
     if (props.id) {
         return (
             <Heading {...props}>
-                <a href={`#${props.id}`}>{props.children}</a>
+                <HeadingLink href={`#${props.id}`}>{props.children}</HeadingLink>
             </Heading>
         )
     }
