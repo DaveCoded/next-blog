b4501503cf0c471e050cf01cbf40238541abc80b
Dave Bernhard <davidbernhard@live.com>
2022-07-24 12:20:04 -0400

    clean up ReplacementComponents

--- components/mdx/ReplacementComponents.tsx
+++ components/mdx/ReplacementComponents.tsx
@@ -6,17 +6,9 @@ import React, {
     AnchorHTMLAttributes
 } from 'react'
 import styled, { StyledComponent } from 'styled-components'
-import bidirectionalLinksData from 'links.json'
 import { Blockquote, CodeBlock, H2, H3, H4, H5, P } from '@/components/mdx/typography'
 import ExternalLink from '@/components/ExternalLink'
-import BacklinkTooltip from './BacklinkTooltip'
-import InternalLink from './InternalLink'
-
-import { unified } from 'unified'
-import markdown from 'remark-parse'
-import html from 'rehype-stringify'
-import remark2rehype from 'remark-rehype'
-import rehypeTruncate from 'rehype-truncate'
+import LinkWithTooltip from './custom/LinkWithTooltip'
 
 const UL = styled.ul`
     padding-inline-start: 1.5rem;
@@ -33,26 +25,6 @@ const ImgContainer = styled.div`
 const HeadingLink = styled.a`
     &:hover {
         color: currentColor;
-    }
-`
-
-const TooltipTitle = styled.h3`
-    margin-bottom: var(--space-sm);
-    color: #edf1f6;
-    font-size: var(--text-md);
-    font-family: 'Headline';
-    line-height: 1.125;
-`
-
-const Excerpt = styled.div`
-    p {
-        color: #edf1f6;
-        font-size: 1.05rem;
-        line-height: 1.3;
-
-        &:last-child {
-            margin-bottom: 0;
-        }
     }
 `
 
@@ -91,35 +63,7 @@ const ReplacementComponents = {
     a: (props: AnchorHTMLAttributes<HTMLAnchorElement>) => {
         const { href, className, children } = props
         if (className?.includes('post-backlink')) {
-            // This relies on the options passed to the remark plugin in [slug].tsx
-            const alias = href?.split('#/page/')[1]
-            if (!alias || !bidirectionalLinksData) return <>children</>
-
-            const linkData = bidirectionalLinksData.find((post) => post.ids.includes(alias))
-            const { slug, title, content } = linkData || { slug: '' }
-
-            const previewHtml = unified()
-                .use(markdown)
-                .use(remark2rehype)
-                .use(rehypeTruncate, { maxChars: 150 })
-                .use(html)
-                .processSync(content)
-                .toString()
-
-            console.log({ previewHtml })
-
-            return (
-                <BacklinkTooltip
-                    content={
-                        <div>
-                            <TooltipTitle>{title}</TooltipTitle>
-                            <Excerpt dangerouslySetInnerHTML={{ __html: previewHtml }} />
-                        </div>
-                    }
-                >
-                    <InternalLink href={`/blog/${slug}`}>{children}</InternalLink>
-                </BacklinkTooltip>
-            )
+            return <LinkWithTooltip href={href}>{children}</LinkWithTooltip>
         }
         if (href?.startsWith('#')) {
             return <a style={{ color: 'var(--link-pink)' }} {...props} />
