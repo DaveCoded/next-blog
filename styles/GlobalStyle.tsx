8fc79b015a47abe46c98a10e2778f9fc0174fc15
Dave Bernhard <davidbernhard@live.com>
2023-10-14 07:18:21 +0100

    Revert "Merge pull request #34 from DaveCoded/swich-font"
    This reverts commit 7b777e535b875a299fed191d571c309bc7c9869b, reversing
    changes made to 5d33df147ff6ab425e4fd6d73ebc4f7924362228.

--- styles/GlobalStyle.tsx
+++ styles/GlobalStyle.tsx
@@ -84,7 +84,7 @@ export const GlobalStyle = createGlobalStyle`
     body {
         padding: 0;
         margin: 0;
-        font-family: sans-serif;
+        font-family: 'Averta', sans-serif;
     }
 
     body {
